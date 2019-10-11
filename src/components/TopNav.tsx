417e7ad496e8eedbd00c54f77d10b1b822bd296b
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-10 22:12:03 -0700

    Begin topnav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -6,7 +6,13 @@ interface IProps {
 
 const TopNav = (props: IProps) => {
     return (
-        <nav id='topnav' className='topnav'></nav>
+        <nav id='topnav' className='topnav'>
+            <div className='topnav__logo'><img src='/src/assets/images/ft-white.png' /></div>
+            <ul className='topnav__menu_list'>
+                <li>What We Do</li>
+                <li>Our Story</li>
+            </ul>
+        </nav>
     )
 }
 
