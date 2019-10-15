7a6163d380af5bb02ad259274d71e9bb32d512a6
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-14 22:09:40 -0700

    Testimonials added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -48,20 +48,38 @@ class App extends React.Component<{}, IState> {
 
     render() {
         return (
-            <>
+            <div className='site-wrap'>
                 <MenuWidget open={this.state.menuOpen} onClose={this.handleMenuClose} />
                 <TopNav visible={this.state.showTopNav} onMenuOpen={this.handleMenuOpen} />
                 <Hero onMenuOpen={this.handleMenuOpen} />
                 <WhatWeDo />
-                <OurStory />
                 <Testimonial
                     name='Jeff Laird'
-                    source='Teacher, Oak Bay Secondary School'
+                    source='Teacher, School District 61'
                     image='/src/assets/images/testimonials/jeff.jpg'
                     quote={`It's been called "the gift of time" at Oak Bay - to let kids do what they're excited about, what they're passionate about and to get the learning support that they need from teachers. Spotlight is a great accountability mechanism. It's great being able to see where kids are going and to help kids plan out their days and take responsibility for their learning.`}
                 />
+                <OurStory />
+                <Testimonial
+                    name='Ciaran McLaverty'
+                    source={`Vice Principal, Island Catholic Schools`}
+                    image='/src/assets/images/testimonials/ciaran.jpg'
+                    quote={`The great thing is that any teacher anywhere can check if a student has signed up for a block somewhere, and it's a great feature to have. Student accountability has been great; with some students missing these blocks, teachers being able to look a student up and generate a report fixes a lot of problems for us.`}
+                />
                 <Spotlight />
-            </>
+                <Testimonial
+                    name='Mike Bobbit'
+                    source={`Vice Principal, School District 62`}
+                    image='/src/assets/images/testimonials/mike.jpg'
+                    quote={`For us it's been amazing. We can run larger sessions where kids come in and it works like a charm. I like being able to schedule months in advance, and Spotlight allows me to do that. Our staff wanted to make sure they could hold kids accountable for where they were and what they were doing, and Spotlight manages that for us.`}
+                />
+                <Testimonial
+                    name='Melanie Paas'
+                    source={`Vice Principal, School District 61`}
+                    image='/src/assets/images/testimonials/melanie.jpg'
+                    quote={`The opportunity for students to have focus time that is their choice within their school day to actually get work done is such a gift. This is the perfect solution where they have responsibility and we are able to account for their time as we need to required by ministry but still gives them the flexibility of choice.`}
+                />
+            </div>
         )
     }
 }
--- src/components/Sections/Spotlight.tsx
+++ src/components/Sections/Spotlight.tsx
@@ -2,12 +2,66 @@ import * as React from 'react'
 
 const Spotlight = () => {
     return (
-        <section className='section --fit-content'>
-            <div className='section__inner'>
-                <h1>Spotlight</h1>
-                <h2>Smart attendance for focus blocks.</h2>
-            </div>
-        </section>
+        <>
+            <section className='section --fit-content'>
+                <div className='section__inner'>
+                    <h1>Spotlight</h1>
+                    <h2>Smart attendance for focus blocks.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis. Pellentesque eu elit et dui pulvinar ullamcorper sit amet semper eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </section>
+            <section className='section --fit-content'>
+                <div className='section__inner'>
+                    <div className='flexbox'>
+                        <div>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                        </div>
+                        <div>
+                            <img alt='image1' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='section --fit-content'>
+                <div className='section__inner'>
+                    <div className='flexbox'>
+                        <div>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                        </div>
+                        <div>
+                            <img alt='image1' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='section --fit-content'>
+                <div className='section__inner'>
+                    <div className='flexbox'>
+                        <div>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                        </div>
+                        <div>
+                            <img alt='image1' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
