a81e4aab96f8a7c49b5ecd7d2f017a1d1f665b88
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-15 01:49:24 -0700

    Add contact us image

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react'
 
+import { Button } from '../Button'
+
 const Contact = () => {
     return (
         <section className='section about_section --fit-content'>
             <div className='section__inner'>
-                <h1>Contact</h1>
-                <h2>Got a question? Ask us! Want a demo? Let’s book a time.</h2>
+                <div className='flexbox'>
+                    <div>
+                        <img src='/src/assets/images/hero/undraw_contact_us_15o2.svg' />
+                    </div>
+                    <div>
+                        <div className='section__inner'>
+                            <h1>Contact</h1>
+                            <h2>Got a question? Ask us! Want a demo? Let’s book a time.</h2>
+                            <Button to='contact'>Contact Us</Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/components/Sections/WhatWeDo.tsx
+++ src/components/Sections/WhatWeDo.tsx
@@ -8,7 +8,8 @@ const WhatWeDo = () => {
                 <div className='flexbox'>
                     <div>
                         <h2>More than just software.</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis. Pellentesque eu elit et dui pulvinar ullamcorper sit amet semper eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut justo sit amet arcu dictum laoreet. Ut porttitor augue in orci mollis, sed accumsan eros scelerisque. Nulla et augue est. Nam malesuada urna tortor, eu imperdiet dui ornare ac. Vestibulum sit amet risus libero. Sed vulputate justo neque, venenatis viverra metus ornare ut. Suspendisse hendrerit cursus mauris sed porta. Duis fermentum bibendum magna, rutrum tristique nibh tincidunt in. Nunc molestie rutrum purus et mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis. Pellentesque eu elit et dui pulvinar ullamcorper sit amet semper eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut justo sit amet arcu dictum laoreet.</p>
+                        <a className='link'>Read More</a>
                     </div>
                     <div>
                         <img src='/src/assets/images/hero/undraw_teacher_35j2.svg' />
