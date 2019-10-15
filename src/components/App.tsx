70ee9d287959e673b154f94d859c03891e148715
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-15 12:52:14 -0700

    Implement React Router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,11 @@
 import * as React from 'react'
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	RouteComponentProps,
+	Switch
+} from 'react-router-dom'
 import '../assets/styles/main.scss'
 
 import LandingPage from './Pages/LandingPage'
@@ -6,9 +13,12 @@ import LandingPage from './Pages/LandingPage'
 class App extends React.Component {
     render() {
         return (
-            <>
-                <LandingPage />
-            </>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={LandingPage} />
+                    <Route render={() => <Redirect to='/' />} />
+                </Switch>
+            </Router>
         )
     }
 }
