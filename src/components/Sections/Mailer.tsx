6f92cb277ad8ad0d14249907915f99102b59f43d
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-14 23:08:18 -0700

    Create mailer section

---
+++ src/components/Sections/Mailer.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+
+import { Button } from '../Button'
+
+const Mailer = () => {
+    const [inputValue, setInputValue] = React.useState('')
+
+    return (
+        <section className='section mailer_section --fit-content'>
+            <div className='section__inner'>
+                <h2>Start using powerful tools that let your self directed study blocks succeed.</h2>
+                <div className='textfield'>
+                    <input
+                        type='text'
+                        placeholder='Your email'
+                        value={inputValue}
+                        onChange={(event: any) => setInputValue(event.target.value)}
+                    />
+                    <Button onPrimary>Keep Me Posted</Button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Mailer
--- src/components/Sections/Spotlight.tsx
+++ src/components/Sections/Spotlight.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import Mailer from './Mailer'
+
 const Spotlight = () => {
     return (
         <>
@@ -8,8 +10,10 @@ const Spotlight = () => {
                     <h1>Spotlight</h1>
                     <h2>Smart attendance for focus blocks.</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis. Pellentesque eu elit et dui pulvinar ullamcorper sit amet semper eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <a className='link'>Book a Demo</a>
                 </div>
             </section>
+            <Mailer />
             <section className='section --fit-content'>
                 <div className='section__inner'>
                     <div className='flexbox'>
@@ -31,15 +35,15 @@ const Spotlight = () => {
                 <div className='section__inner'>
                     <div className='flexbox'>
                         <div>
-                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
-                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
-                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <img alt='image1' />
                         </div>
                         <div>
-                            <img alt='image1' />
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
+                            <h2>Pellentesque eu elit et dui pulvinar ullamcorper.</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur metus a elit dignissim, congue egestas ex convallis.</p>
                         </div>
                     </div>
                 </div>
