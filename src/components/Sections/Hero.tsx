e1117854f4a1e10f3dd30e535179401043394704
Curtis Upshall <curtisupshall@uvic.ca>
2019-11-03 16:42:28 -0800

    Change hero layout

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -22,22 +22,23 @@ const Hero = (props: IProps) => {
         return (
             <section id='hero' className={classNames('section', 'hero_section')}>
                 <TopNav onHero visible onMenuOpen={props.onMenuOpen} />
-                <div className='section__inner'>
-                    <div className='flexbox'>
-                        <div>
-                            <div className='focus'>
-                                <h1 className='focus__header'>Focus.</h1>
-                                <h6 className='focus__subheader'>
-                                    <Loop noWrap={false} children={messages} />
-                                </h6>
-                            </div>
+                <div className='section__inner hero__container'>
+                    <div className='focus'>
+                        <h1 className='focus__header'>Focus.</h1>
+                        <h6 className='focus__subheader'>
+                            <Loop noWrap={true} children={messages} />
+                        </h6>
+                    </div>
+                    <div className='summary'>
+                        <div className='summary__info'>
                             <h6>We create platforms where educators and students can share information and get smarter together.</h6>
                             <Button onPrimary>Book a Demo</Button>
+                        </div>   
+                        <div className='summary__image'>
+                            <img className='hero_image' src='src/assets/images/hero/undraw_new_ideas_jdea (1).svg'/>
                         </div>
-                        <div><img className='hero_image' src='src/assets/images/hero/undraw_new_ideas_jdea (1).svg'/></div>
                     </div>
-                    
-                </div>
+                </div>                               
             </section>
         )
 
