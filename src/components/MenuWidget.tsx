b5aadf5d3665817fc9ef2e300d1a537496791cf3
Curtis Upshall <curtisupshall@uvic.ca>
2019-11-04 16:52:54 -0800

    Menu links

--- src/components/MenuWidget.tsx
+++ src/components/MenuWidget.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react'
 import * as classNames from 'classnames'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 interface IProps {
     open: boolean
@@ -17,10 +18,10 @@ const MenuWidget = (props: IProps) => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                         </a>
                     </div>
-                    <ul>
-                        <li>What We Do</li>
-                        <li>Our Story</li>
-                        <li>Spotlight</li>
+                    <ul className='menu__list'>
+                        <Link to='our-story'><li>Our Story</li></Link>
+                        <Link to='spotlight'><li>Spotlight</li></Link>
+                        <Link to='contact'><li>Contact</li></Link>
                     </ul>
                 </div>
             </div>
