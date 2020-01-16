038ee0daa706e463517c25c5b667be549262e554
Curtis Upshall <curtisupshall@uvic.ca>
2020-01-16 10:10:43 -0800

    Add Google site verification meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
                 <Head>
                     <meta charSet='UTF-8' />
                     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                    <meta name="google-site-verification" content="0DcKBYV4vWtx6YTcSxXTIHA05BODKtEnCuZc1BH7U2E" />
                     <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
                     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap' rel='stylesheet' />
                 </Head>
