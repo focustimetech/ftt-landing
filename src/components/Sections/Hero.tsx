6a8585bab64bd8d40ad13b7265012b6cb1e8c80f
Curtis Upshall <curtisupshall@gmail.com>
2025-07-16 16:33:57 -0700

    Adds react-text-loop

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import React from 'react'
 
 import { Button } from '@mui/material'
 
 import TopNav from '../TopNav'
+import { TextLoop } from '@pr0gramm/react-text-loop'
 
 interface HeroProps {
     texts: string[]
@@ -15,9 +18,16 @@ const Hero = (props: HeroProps) => {
             <div className='section__inner hero__container'>
                 <div className='hero_summary'>
                     <div className='focus'>
-                        <h6>Focus on</h6>
-                        <h6 className='loop'>
-                            {props.texts[0]}
+                        <h6>
+                            <span>Focus on</span>
+                            <br />
+                            <TextLoop>
+                                {props.texts.map((text, index) => (
+                                    <span key={index} className='loop'>
+                                        {text}
+                                    </span>
+                                ))}
+                            </TextLoop>
                         </h6>
                     </div>
                     <h6 className='tagline'>We create platforms where educators and students can share information and get smarter together.</h6>
