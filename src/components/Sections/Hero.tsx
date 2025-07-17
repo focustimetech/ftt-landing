132b6d0c6134bc6a8d04c613244a79cc34a53344
Curtis Upshall <curtisupshall@gmail.com>
2025-07-16 19:35:02 -0700

    Fixes hero section formatting

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -18,22 +18,25 @@ const Hero = (props: HeroProps) => {
     return (
         <section id='hero' className='section hero_section --fit-content --topnav'>
             <TopNav onHero static />
+            <h1 className='hero_title'>
+                FocusTime
+            </h1>
             <div className='section__inner hero__container'>
                 <div className='hero_summary'>
                     <div className='focus'>
-                        <h6>
-                            <span>Focus on</span>
-                            <br />
-                            <TextLoop>
-                                {props.texts.map((text, index) => (
-                                    <span key={index} className='loop'>
-                                        {text}
-                                    </span>
-                                ))}
-                            </TextLoop>
-                        </h6>
+                        <span>Focus on</span>
+                        <br />
+                        <TextLoop interval={2000}>
+                            {props.texts.map((text, index) => (
+                                <span key={index} className='loop'>
+                                    {text}
+                                </span>
+                            ))}
+                        </TextLoop>
                     </div>
-                    <h6 className='tagline'>We create platforms where educators and students can share information and get smarter together.</h6>
+                    <p className='tagline'>
+                        We create platforms where educators and students can share information and get smarter together.
+                    </p>
                     {!disableCta && (
                         <Button variant='contained' color='secondary' href='/contact'>Book a Demo</Button>
                     )}
